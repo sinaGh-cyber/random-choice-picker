01c62165e2d0bacac4d37b48cbf22c08a4634bd4
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2021-10-17 01:36:41 +0330

    creating tags func added

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+const tagsEl = document.getElementById('tags');
+const textarea = document.getElementById('textarea');
 
+const createTags = (input) => {
+  const tags = input
+    .split(',')
+    .filter((val) => {
+      return val.trim() !== '';
+    })
+    .map((val) => val.trim());
+
+  tagsEl.innerHTML = '';
+
+  tags.forEach((tag) => {
+    const tagEl = document.createElement('span');
+    tagEl.classList.add('tag');
+    tagEl.innerHTML = tag;
+    tagsEl.appendChild(tagEl);
+  });
+};
+
+textarea.focus();
+
+textarea.addEventListener('keyup', (e) => {
+  createTags(e.target.value);
+});
